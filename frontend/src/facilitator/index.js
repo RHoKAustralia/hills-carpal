b49303a26a5fa12cb1ced2ca166ff0fea2e32198
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2018-06-02 22:57:00 +1000

    Fetch fake data in a more realish way

--- frontend/src/facilitator/index.js
+++ frontend/src/facilitator/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Table from '../components/table';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const columns = [
   { dataField: 'client', text: 'client' },
@@ -26,15 +27,28 @@ const columns = [
   { dataField: 'status', text: 'Status' },
 ];
 class Facilitator extends React.Component {
+  constructor() {
+    super();
+    this.state = { drives: null };
+  }
+  componentDidMount() {
+    // const url = process.env.REACT_APP_API_URL + '/drives'
+    axios.get('sampledata.json').then(res => {
+      this.setState({ drives: res.data });
+    });
+  }
   render() {
+    if (!this.state.drives) {
+      return <img alt="loader" className="loader" src="loader.svg" />;
+    }
+
     return (
       <div className="container">
         <h1>Rides</h1>
-
         <Link className="btn btn-primary" to={'/facilitator/create'}>
           Create new
         </Link>
-        <Table columns={columns} />
+        <Table data={this.state.drives} columns={columns} />
       </div>
     );
   }
