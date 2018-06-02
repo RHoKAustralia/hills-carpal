3b6aa0db9d67cfd2d94335df2d9ddab9e0220561
douglas <douglascvas@gmail.com>
2018-06-02 17:41:41 +1000

    Fixed API to create rides

--- infrastructure/services/rides/create.js
+++ infrastructure/services/rides/create.js
@@ -3,7 +3,7 @@
 const uuid = require('uuid'); // eslint-disable-line import/no-extraneous-dependencies
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 const validate = require('jsonschema').validate; // eslint-disable-line import/no-extraneous-dependencies
-const rideSchema = require('../schema/ride'); // eslint-disable-line import/no-extraneous-dependencies
+const rideSchema = require('../schema/ride.json'); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS
   .DynamoDB
@@ -17,13 +17,13 @@ module.exports.create = (event, context, callback) => {
   data.id = uuid.v1();
   const validationResult = validate(data, rideSchema);
 
-  if (validationResult) {
+  if (validationResult.errors && validationResult.errors.length) {
     callback(null, {
       statusCode: 400,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'text/plain'
       },
-      body: {"error": validationResult}
+      body: JSON.stringify({"error": validationResult.errors})
     });
     return;
   }
@@ -40,9 +40,9 @@ module.exports.create = (event, context, callback) => {
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'text/plain'
         },
-        body: {dbError: error}
+        body: JSON.stringify({dbError: error})
       });
       return;
     }
@@ -50,7 +50,7 @@ module.exports.create = (event, context, callback) => {
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(params.Item)
+      body: JSON.stringify(data)
     };
     callback(null, response);
   });
