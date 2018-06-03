a62cdf68724c39b163ea720914c9a3d0fa97a7b5
ChristopherForrest <Christopherforrest92@gmail.com>
2018-06-03 14:22:26 +1000

    fix import

--- frontend/src/auth/Login.js
+++ frontend/src/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './login.css'
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   login() {
