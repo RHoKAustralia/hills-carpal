015d2372fde65f639869f9d1697d126cc7d276ad
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2018-06-03 14:40:59 +1000

    Only style login

--- frontend/src/auth/Login.js
+++ frontend/src/auth/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './login.css'
+import './login.css';
 import { Link } from 'react-router-dom';
 
 class Login extends Component {
@@ -9,77 +9,67 @@ class Login extends Component {
   }
 
   facilitatorLink() {
-    const isAuthorised = this.props.auth.hasFacilitatorPriviledge() || this.props.auth.hasAdminPriviledge();
+    const isAuthorised =
+      this.props.auth.hasFacilitatorPriviledge() ||
+      this.props.auth.hasAdminPriviledge();
     if (isAuthorised) {
-      return <li>
-        <Link to="/facilitator">Facilitator</Link>
-      </li>
+      return (
+        <li>
+          <Link to="/facilitator">Facilitator</Link>
+        </li>
+      );
     } else {
       return false;
     }
   }
 
   driverLink() {
-    const isAuthorised = this.props.auth.hasDriverPriviledge() || this.props.auth.hasAdminPriviledge();
+    const isAuthorised =
+      this.props.auth.hasDriverPriviledge() ||
+      this.props.auth.hasAdminPriviledge();
     if (isAuthorised) {
-      return <li>
-        <Link to="/driver">Driver</Link>
-      </li>
+      return (
+        <li>
+          <Link to="/driver">Driver</Link>
+        </li>
+      );
     } else {
       return false;
     }
   }
 
   render() {
-    const { isAuthenticated, hasAdminPriviledge } = this.props.auth;
+    const { isAuthenticated } = this.props.auth;
     return (
-
-
-
-      //header
-      <div>
-        <div className="Login-header">
-  
-        </div>
+      <div className="login-container">
+        <div className="Login-header" />
         <div>
-
-
-
-      
-          {
-            !isAuthenticated() && (
-          
+          {!isAuthenticated() && (
             <div class="row">
               <div class="col-md-12 text-center">
-             <div className="outerForm">
-              <div className="quote">Share the ride, share the life</div>
-              
-                <button
-                  className="btn btn-success btn-block"
-                  id="loginButton"
-                  onClick={this.login.bind(this)}
-                >
-                  Log In
-                </button>
+                <div className="outerForm">
+                  <div className="quote">Share the ride, share the life</div>
+
+                  <button
+                    className="btn btn-success btn-block"
+                    id="loginButton"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </button>
                 </div>
               </div>
-                </div>
-           
-              )
-          }
-          {
-            isAuthenticated() && (
-              <div>
-                Pick your action:
-                <ul>
-                  {this.facilitatorLink()}
-                  {this.driverLink()}
-                </ul>
-              </div>
-            )
-          }
-
-
+            </div>
+          )}
+          {isAuthenticated() && (
+            <div>
+              Pick your action:
+              <ul>
+                {this.facilitatorLink()}
+                {this.driverLink()}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     );
