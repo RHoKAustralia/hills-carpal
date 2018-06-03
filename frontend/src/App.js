b091a928bf82a5c6f554348b843abe323b90caec
syleung <lemonzpam@hotmail.com>
2018-06-03 10:50:26 +1000

    Adding restriction to view pages base on user's profile role.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userRole: '',
-    };
-  }
-
   logout() {
     this.props.auth.logout();
   }
 
   render() {
-    const { isAuthenticated } = this.props.auth;
+    const { isAuthenticated, getUserRole } = this.props.auth;
     return (
       <div>
         <div className="App-header">
           <div>Hills CarPal</div>
-          <div>
-            This nav is for demo
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/facilitator">Facilitator</Link>
-              </li>
-              <li>
-                <Link to="/driver">Driver</Link>
-              </li>
-            </ul>
-          </div>
           <div className="App-header-controls">
             {
               isAuthenticated() && (
