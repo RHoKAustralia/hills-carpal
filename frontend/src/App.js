7f5eee47ee3cfc49517b61b87d62c384dfb1c8be
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2018-06-04 22:42:21 +1000

    Prettier + class is called classname in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,50 +7,68 @@ class App extends Component {
   }
 
   render() {
- 
     const { isAuthenticated } = this.props.auth;
     return (
-
       <div>
-       <nav className="navbar sticky-top navbar-light bg-light">
-
-     <a className="navbar-brand" href="http://www.hillscarpal.org.au/">
-        <img src="/styles/carpal.png" width="200" height="100" alt="HillsCarPal" id="icon"/>
-     </a>
-     {
-     !isAuthenticated() && (
-      <div className="linkBlock">
-     <a className="links" href="http://www.hillscarpal.org.au/" display ="inline-block">about us |</a>
-     <a className="links" href="http://www.hillscarpal.org.au/" display ="inline-block" > contribute |</a>
-     <a className="donatelink"  href="http://www.hillscarpal.org.au/donate/" display ="inline-block"> donate |</a>
-     <a className="links" href="http://www.hillscarpal.org.au/contact-us/" display ="inline-block"> contact us</a>
-     </div>
-     )
-    }
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="http://www.hillscarpal.org.au/">
+            <img
+              src="/styles/carpal.png"
+              width="200"
+              height="100"
+              alt="HillsCarPal"
+              id="icon"
+            />
+          </a>
+          {!isAuthenticated() && (
+            <div className="linkBlock">
+              <a
+                className="links"
+                href="http://www.hillscarpal.org.au/"
+                display="inline-block"
+              >
+                about us |
+              </a>
+              <a
+                className="links"
+                href="http://www.hillscarpal.org.au/"
+                display="inline-block"
+              >
+                contribute |
+              </a>
+              <a
+                className="donatelink"
+                href="http://www.hillscarpal.org.au/donate/"
+                display="inline-block"
+              >
+                donate |
+              </a>
+              <a
+                className="links"
+                href="http://www.hillscarpal.org.au/contact-us/"
+                display="inline-block"
+              >
+                {' '}
+                contact us
+              </a>
+            </div>
+          )}
           <div className="App-header-controls">
-           
-            {
-              isAuthenticated() && (
-                  <button
-                  class="btn btn-success"
-                  id="logOutButton"
-                    onClick={this.logout.bind(this)}
-                  >
-                    Log Out
-                  </button>
-                
-                )
-            }
+            {isAuthenticated() && (
+              <button
+                className="btn btn-success"
+                id="logOutButton"
+                onClick={this.logout.bind(this)}
+              >
+                Log Out
+              </button>
+            )}
           </div>
-          
-       </nav>
-
+        </nav>
 
         {this.props.children}
       </div>
-    
     );
-  
   }
 }
 
