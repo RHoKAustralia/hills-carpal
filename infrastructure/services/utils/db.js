91181ea57e14dc26e5c9b84d695914fc3d46aa1d
lthompson <lthompson@infomedia.com.au>
2018-06-25 09:22:51 +1000

    Externalise the port config for MySQL + move to secret port

--- infrastructure/services/utils/db.js
+++ infrastructure/services/utils/db.js
@@ -1,3 +1,11 @@
 var mysql = require('mysql');
 const MYSQL_PW = process.env.MYSQL_PW;
-module.exports.connection = () => mysql.createConnection({host: 'carpal.cttgjqpjknhf.ap-southeast-2.rds.amazonaws.com', user: 'carpaladmin', password: MYSQL_PW, database: 'carpal'});
+const MYSQL_PORT = process.env.MYSQL_PORT;
+
+module.exports.connection = () => mysql.createConnection({
+    host: 'carpal.cttgjqpjknhf.ap-southeast-2.rds.amazonaws.com', 
+    port: MYSQL_PORT, 
+    user: 'carpaladmin', 
+    password: MYSQL_PW, 
+    database: 'carpal'
+});
