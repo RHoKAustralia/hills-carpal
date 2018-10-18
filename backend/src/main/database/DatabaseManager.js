a24fa3a69414792b93e2f48a0876715fbd9b6e50
Alex Gilleran <AlexGilleran@users.noreply.github.com>
2018-10-18 23:45:25 +1100

    Update DatabaseManager.js

--- backend/src/main/database/DatabaseManager.js
+++ backend/src/main/database/DatabaseManager.js
@@ -7,7 +7,7 @@ class DatabaseManager {
       host: process.env.MYSQL_HOST || 'localhost',
       port: process.env.MYSQL_PORT || 3306,
       user: process.env.MYSQL_USER || 'root',
-      password: process.env.MYSQL_PW || '',
+      password: process.env.MYSQL_PW || 'admin',
       database: process.env.MYSQL_DB || 'carpal',
       multipleStatements: true
     };
