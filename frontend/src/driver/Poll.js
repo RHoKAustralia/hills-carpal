aa3fe36bc5867170b88fb6f50729c8138af5a752
Alex Gilleran <alex.gilleran@nicta.com.au>
2019-06-22 23:30:42 +1000

    Added poll for ride completion

---
+++ frontend/src/driver/Poll.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import axiosInstance from '../auth/api';
+import { Link } from 'react-router-dom';
+
+export default class Poll extends React.Component {
+  loadCount = 0;
+
+  state = {
+    submitState: 'form' // "form" | "saving" | "done" | "error"
+  };
+
+  onIFrameLoad = () => {
+    this.loadCount++;
+
+    if (this.loadCount >= 2) {
+      this.setState({
+        submitState: 'saving'
+      });
+
+      // set ride to complete
+      axiosInstance
+        .put(
+          `${process.env.REACT_APP_API_URL}/rides/${
+            this.props.match.params.rideId
+          }`,
+          {
+            status: 'ENDED'
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('id_token')}`
+            }
+          }
+        )
+        .then(() => {
+          this.setState({
+            submitState: 'done'
+          });
+        })
+        .catch(e => {
+          console.error(e);
+          this.setState({
+            submitState: 'error'
+          });
+        });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {(() => {
+          if (this.state.submitState === 'form') {
+            return (
+              <iframe
+                frameborder="0"
+                width="100%"
+                height="600"
+                scrolling="auto"
+                allowtransparency="true"
+                src="https://alexgilleran.survey.fm/test-poll-please-ignore?iframe=1"
+                onLoad={this.onIFrameLoad}
+              >
+                <a href="https://alexgilleran.survey.fm/test-poll-please-ignore">
+                  View Survey
+                </a>
+              </iframe>
+            );
+          } else if (this.state.submitState === 'saving') {
+            return 'Saving...';
+          } else if (this.state.submitState === 'error') {
+            return 'Error';
+          } else {
+            return (
+              <div>
+                Thanks! <Link to="/driver/find-ride">Find another ride</Link>
+              </div>
+            );
+          }
+        })()}
+      </div>
+    );
+  }
+}
