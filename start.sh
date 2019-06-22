2bbe331c9a5159635c3624df898d0ffc0e7dfc52
Damien Biasotto <damien.biasotto@rezdy.com>
2019-06-22 11:02:11 +1000

    Make starting  the app a abit easier for new developers.

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Starting database"
+docker run --name hills-carpal -p 3306:3306 -e MYSQL_ROOT_PASSWORD=admin -d mysql:5.7
+
+echo "Starting backend"
+cd backend
+yarn
+yarn run refresh-db
+UNSAFE_GOD_MODE=true yarn start
+
+echo "Starting frontend"
+cd -
+cd frontend
+yarn
+yarn start
