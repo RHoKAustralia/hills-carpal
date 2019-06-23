df3c20c5dc01bf76a9b209e49eb5b3a917d58920
Damien Biasotto <damien.biasotto@rezdy.com>
2019-06-23 11:22:02 +1000

    Update driver_ride table structure
    to have a composed primary key,
    so we can leverage on it for insert / update on duplicate key single query.

--- backend/src/main/database/changes/2019-06-22-7-db-cleanup.sql
+++ backend/src/main/database/changes/2019-06-22-7-db-cleanup.sql
@@ -8,21 +8,13 @@ DROP table location;
 
 create table driver_ride
 (
-    email varchar(255) not null,
+    driver_email varchar(255) not null,
     ride_id int not null,
-    confirmed tinyint default null null comment 'IF null driver didn''t accept nor decline the rid.',
-    updated_at DATETIME not null
-);
-
-create unique index driver_ride_email_uindex
-    on driver_ride (driver_email);
-
-alter table driver_ride
-    add constraint driver_ride_pk
-        primary key (driver_email);
-
-alter table driver_ride
-    add constraint driver_ride_rides_id_fk
+    confirmed tinyint null,
+    updated_at DATETIME null,
+    constraint driver_ride_pk
+        primary key (driver_email, ride_id),
+    constraint driver_ride_rides_id_fk
         foreign key (ride_id) references rides (id)
-            on delete cascade;
+);
 
