6899af2ad219f202952fb6f916477c2e16179492
Alex Gilleran <alex@alexgilleran.com>
2019-07-21 23:33:42 +1000

    Able to get 1/5 of a jpg uploaded.

--- backend/src/main/images/ImageRepository.js
+++ backend/src/main/images/ImageRepository.js
@@ -8,18 +8,36 @@ class ClientRepository {
     this._dbName = this._databaseManager.databaseConfig.database;
   }
 
-  upload(content, mimeType, clientId, connection) {
-    let query = `INSERT INTO ${
-      this._dbName
-    }.images(mime_type, content, client_id) 
+  async upload(content, mimeType, clientId, connection) {
+    try {
+      await this._databaseManager.beginTransaction(connection);
+
+      const addImageQuery = `INSERT INTO ${
+        this._dbName
+      }.images(mime_type, content) 
         VALUES (${[
           connection.escape(mimeType),
-          connection.escape(content),
-          connection.escape(clientId)
+          connection.escape(content)
         ].join(',')})`;
-    console.log(query);
 
-    return this._databaseManager.query(query, connection);
+      await this._databaseManager.query(addImageQuery, connection);
+
+      const addJoinQuery = `INSERT INTO ${
+        this._dbName
+      }.client_images(images_id, clients_id) 
+        VALUES (
+          (SELECT LAST_INSERT_ID()),  
+          ${[connection.escape(clientId)].join(',')}
+        )`;
+
+      await this._databaseManager.query(addJoinQuery, connection);
+
+      await this._databaseManager.commit(connection);
+    } catch (e) {
+      await this._databaseManager.rollback(connection);
+      console.error(e);
+      throw e;
+    }
   }
 
   // update(id, client, connection) {
