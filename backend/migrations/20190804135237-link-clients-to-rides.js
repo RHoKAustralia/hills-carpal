63c003bd0416a1576381c4f30a705ac5b743fab2
Alex Gilleran <alex@alexgilleran.com>
2019-08-05 20:59:41 +1000

    Correction formatting.

--- backend/migrations/20190804135237-link-clients-to-rides.js
+++ backend/migrations/20190804135237-link-clients-to-rides.js
@@ -8,9 +8,9 @@ var path = require('path');
 var Promise;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
@@ -19,35 +19,41 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190804135237-link-clients-to-rides-up.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+  var filePath = path.join(
+    __dirname,
+    'sqls',
+    '20190804135237-link-clients-to-rides-up.sql'
+  );
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
       if (err) return reject(err);
       console.log('received data: ' + data);
 
       resolve(data);
     });
-  })
-  .then(function(data) {
+  }).then(function(data) {
     return db.runSql(data);
   });
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190804135237-link-clients-to-rides-down.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+  var filePath = path.join(
+    __dirname,
+    'sqls',
+    '20190804135237-link-clients-to-rides-down.sql'
+  );
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
       if (err) return reject(err);
       console.log('received data: ' + data);
 
       resolve(data);
     });
-  })
-  .then(function(data) {
+  }).then(function(data) {
     return db.runSql(data);
   });
 };
 
 exports._meta = {
-  "version": 1
+  version: 1
 };
