bb7d2282598b14c1e2b037cc2bbc8bf4a5d808ce
Alex Gilleran <alex.gilleran@nicta.com.au>
2019-08-11 11:44:25 +1000

    Committing the auth0 rule.

---
+++ auth0rule.js
@@ -0,0 +1,36 @@
+// This scoops data out of the auth0 rules and puts into the JWT so we can use it.
+
+function (user, context, callback) {
+    const namespace = 'https://carpal.org.au';
+    const assignedRoles = (context.authorization || {}).roles;
+    
+    let idTokenClaims = context.idToken || {};
+    let accessTokenClaims = context.accessToken || {};
+    
+    function setClaim(claimId, value) {
+      idTokenClaims[`${namespace}/${claimId}`] = value;
+      accessTokenClaims[`${namespace}/${claimId}`] = value;
+    }
+  
+    setClaim("roles", assignedRoles);
+    
+    const roleLookup = assignedRoles.reduce((soFar, role) => {
+      soFar[role] = true;
+      return soFar;
+    }, {});
+    
+    if (roleLookup.male) {
+      setClaim("gender", "male");
+    } else if (roleLookup.female) {
+      setClaim("gender", "female");
+    }
+    
+    if (roleLookup.suv) {
+      setClaim("car", "suv");
+    }
+  
+    context.idToken = idTokenClaims;
+    context.accessToken = accessTokenClaims;
+  
+    callback(null, user, context);
+  }
