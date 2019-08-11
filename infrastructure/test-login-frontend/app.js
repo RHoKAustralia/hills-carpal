b251c2c61a0887db5bd02f39b36fbd069d9cc4e8
Alex Gilleran <alex.gilleran@nicta.com.au>
2019-08-11 10:56:21 +1000

    Added better error handling.

--- infrastructure/test-login-frontend/app.js
+++ infrastructure/test-login-frontend/app.js
@@ -4,11 +4,13 @@
 const AUTH0_CLIENT_ID = 'KcWs7TkXHZZ41Co5xhpPR1Oq3nfr4HPW';
 const AUTH0_DOMAIN = 'carpal.au.auth0.com';
 const AUTH0_CALLBACK_URL = window.location.href; // eslint-disable-line
-const LOGGEDIN_URL = 'https://tn99ubmph1.execute-api.ap-southeast-2.amazonaws.com/dev/authcheck';
+const LOGGEDIN_URL =
+  'https://tn99ubmph1.execute-api.ap-southeast-2.amazonaws.com/dev/authcheck';
 // const LOGGEDIN_URL =
 // 'https://m62upgxr2k.execute-api.ap-southeast-2.amazonaws.com/dev/authcheck';
 // initialize auth0 lock
-const lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, { // eslint-disable-line no-undef
+const lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+  // eslint-disable-line no-undef
   auth: {
     params: {
       scope: 'openid email'
@@ -16,12 +18,13 @@ const lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, { // eslint-disable-li
     responseType: 'token id_token'
   },
   theme: {
-    logo: 'https://lh3.googleusercontent.com/Ba2CsLxqbgNuHOgejaNbWwmXkQov4yle1NuxMwiItBsAkX' +
-        '47GVKZms1aQbKcbpL9IZl3M8xi7u-O5X_9wFX8a1zhHw1ZG_B06Wwx5-0sJcBR-E4CD3WrzHIEf2R0Wo' +
-        'LR-FG5W9fMeuk=s150-p-k'
+    logo:
+      'https://lh3.googleusercontent.com/Ba2CsLxqbgNuHOgejaNbWwmXkQov4yle1NuxMwiItBsAkX' +
+      '47GVKZms1aQbKcbpL9IZl3M8xi7u-O5X_9wFX8a1zhHw1ZG_B06Wwx5-0sJcBR-E4CD3WrzHIEf2R0Wo' +
+      'LR-FG5W9fMeuk=s150-p-k'
   },
   languageDictionary: {
-    title: "CarPal"
+    title: 'CarPal'
   }
 });
 
@@ -29,24 +32,16 @@ function updateUI() {
   const isLoggedIn = localStorage.getItem('id_token');
   if (isLoggedIn) {
     // swap buttons
-    document
-      .getElementById('btn-login')
-      .style
-      .display = 'none';
-    document
-      .getElementById('btn-logout')
-      .style
-      .display = 'inline';
+    document.getElementById('btn-login').style.display = 'none';
+    document.getElementById('btn-logout').style.display = 'inline';
     const profile = JSON.parse(localStorage.getItem('profile'));
     // show username
-    document
-      .getElementById('nick')
-      .textContent = profile.email;
+    document.getElementById('nick').textContent = profile.email;
   }
 }
 
 // Handle login
-lock.on('authenticated', (authResult) => {
+lock.on('authenticated', authResult => {
   console.log(authResult);
   lock.getUserInfo(authResult.accessToken, (error, profile) => {
     if (error) {
@@ -54,9 +49,7 @@ lock.on('authenticated', (authResult) => {
       return;
     }
 
-    document
-      .getElementById('nick')
-      .textContent = profile.nickname;
+    document.getElementById('nick').textContent = profile.nickname;
 
     localStorage.setItem('accessToken', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
@@ -69,38 +62,24 @@ lock.on('authenticated', (authResult) => {
 updateUI();
 
 // Handle login
-document
-  .getElementById('btn-login')
-  .addEventListener('click', () => {
-    lock.show();
-  });
+document.getElementById('btn-login').addEventListener('click', () => {
+  lock.show();
+});
 
 // Handle logout
-document
-  .getElementById('btn-logout')
-  .addEventListener('click', () => {
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('profile');
-    document
-      .getElementById('btn-login')
-      .style
-      .display = 'flex';
-    document
-      .getElementById('btn-logout')
-      .style
-      .display = 'none';
-    document
-      .getElementById('nick')
-      .textContent = '';
-  });
+document.getElementById('btn-logout').addEventListener('click', () => {
+  localStorage.removeItem('id_token');
+  localStorage.removeItem('access_token');
+  localStorage.removeItem('profile');
+  document.getElementById('btn-login').style.display = 'flex';
+  document.getElementById('btn-logout').style.display = 'none';
+  document.getElementById('nick').textContent = '';
+});
 
-document
-  .getElementById('btn-loggedin')
-  .addEventListener('click', () => {
-    // Call private API with JWT in header
-    const token = localStorage.getItem('id_token');
-    /*
+document.getElementById('btn-loggedin').addEventListener('click', () => {
+  // Call private API with JWT in header
+  const token = localStorage.getItem('id_token');
+  /*
    // block request from happening if no JWT token present
    if (!token) {
     document.getElementById('message').textContent = ''
@@ -108,24 +87,20 @@ document
      'You must login to call this protected endpoint!'
     return false
   }*/
-    // Do request to private endpoint
-    fetch(LOGGEDIN_URL, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-      .then(response => response.json())
-      .then((data) => {
-        console.log('Token:', data);
-        document
-          .getElementById('message')
-          .textContent = '';
-        document
-          .getElementById('message')
-          .textContent = data.message;
-      })
-      .catch((e) => {
-        console.log('error', e);
-      });
-  });
+  // Do request to private endpoint
+  fetch(LOGGEDIN_URL, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Token:', data);
+      document.getElementById('message').textContent = '';
+      document.getElementById('message').textContent = data.message;
+    })
+    .catch(e => {
+      console.error(e);
+    });
+});
