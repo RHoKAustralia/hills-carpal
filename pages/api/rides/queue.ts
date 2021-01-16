52ab64b39ef0e23de90d0f6afb5d3f50d0d84429
Alex Gilleran <alex@alexgilleran.com>
2021-01-16 19:50:02 +1100

    Fixed queue showing everyone's rides

--- pages/api/rides/queue.ts
+++ pages/api/rides/queue.ts
@@ -2,28 +2,32 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import RideRepository from '../../../src/server/api/rides/ride-repository';
 import DatabaseManager from '../../../src/server/api/database/database-manager';
+import {
+  decodeJwt,
+  requireDriverPermissions,
+} from '../../../src/server/api/jwt';
 
 const databaseManager = new DatabaseManager();
 const rideRepository = new RideRepository(databaseManager);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { query, method } = req;
-
-  const parsed = {
-    driverId: query.driverId,
-  };
+  const { method } = req;
 
   switch (method) {
     case 'GET':
       const connection = databaseManager.createConnection();
 
       try {
-        const rides = await rideRepository.listForDriver(
-          parsed.driverId as string,
-          'CONFIRMED',
-          connection
-        );
-        res.status(200).json(rides);
+        const jwt = decodeJwt(req);
+
+        if (requireDriverPermissions(jwt, req, res)) {
+          const rides = await rideRepository.listForDriver(
+            jwt.userId,
+            'CONFIRMED',
+            connection
+          );
+          res.status(200).json(rides);
+        }
       } catch (e) {
         res.status(500).json({ status: 'Error' });
       } finally {
