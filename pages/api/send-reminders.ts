d22cb313adfc05337884ee6a1c877f3e749e7bf5
Alex Gilleran <alex@alexgilleran.com>
2021-03-17 00:08:43 +1100

    Reminder for rides that need closing.

--- pages/api/send-reminders.ts
+++ pages/api/send-reminders.ts
@@ -7,6 +7,7 @@ import moment from 'moment';
 import RideRepository from '../../src/server/api/rides/ride-repository';
 import DatabaseManager from '../../src/server/api/database/database-manager';
 import notifyUnclaimedRide from '../../src/server/notifications/notify-unclaimed-ride';
+import notifyRideNeedsClosing from '../../src/server/notifications/notify-ride-needs-closing';
 
 const snsValidator = new SnsMessageValidator();
 const databaseManager = new DatabaseManager();
@@ -75,33 +76,62 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
           // find all rides that are unclaimed and due in a week
           const startOfToday = moment.tz(process.env.TIMEZONE).startOf('day');
-          const fromDate = startOfToday
+          const unclaimedFromDate = startOfToday
             .clone()
             .add(reminderDifferenceDays - 1, 'days');
-          const toDate = startOfToday
+          const unclaimedToDate = startOfToday
             .clone()
             .add(reminderDifferenceDays, 'days');
 
-          const rides = await rideRepository.list(
+          const unclaimedRides = await rideRepository.list(
             {
-              filters: { status: 'OPEN', date: { from: fromDate, to: toDate } },
-              size: Number.MIN_SAFE_INTEGER,
+              filters: {
+                status: 'OPEN',
+                date: { from: unclaimedFromDate, to: unclaimedToDate },
+              },
+              size: Number.MAX_SAFE_INTEGER,
             },
             connection
           );
 
-          console.log(
-            'Received reminder notification, sending reminders for ' +
-              rides.length +
-              ' rides'
+          const closingReminderFromDate = startOfToday.clone().add(-3, 'days');
+          const closingReminderToDate = startOfToday.clone().add(-2, 'days');
+          const closingReminderRides = await rideRepository.list(
+            {
+              filters: {
+                status: 'CONFIRMED',
+                date: {
+                  from: closingReminderFromDate,
+                  to: closingReminderToDate,
+                },
+              },
+              size: Number.MAX_SAFE_INTEGER,
+            },
+            connection
           );
 
           // SNS will only wait 15 seconds before retrying and this might take
           // way longer, so return a 200 before actually sending emails
           res.status(200).send('');
 
-          for (let ride of rides) {
+          console.log(
+            'Received reminder notification, sending reminders for ' +
+              unclaimedRides.length +
+              ' rides'
+          );
+
+          // We do these one by one so that we don't hit a rate limit on SES
+          for (let ride of unclaimedRides) {
             await notifyUnclaimedRide(ride);
+          }
+
+          console.log(
+            'Received unclosed notification, sending reminders for ' +
+              closingReminderRides.length +
+              ' rides'
+          );
+          for (let ride of closingReminderRides) {
+            await notifyRideNeedsClosing(ride);
           }
         }
 
---
+++ src/server/notifications/notify-ride-needs-closing.ts
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import moment from 'moment-timezone';
+
+import { Ride } from '../../common/model';
+import { managementClient } from '../auth/api-auth0';
+import sendEmail from './send-email';
+
+export default async function notifyRideNeedsClosing(ride: Ride) {
+  const driver = await managementClient.getUser({
+    id: ride.driver.id,
+  });
+
+  console.log(
+    `Sending unclosed ride notification for ride ${ride.id} to ${driver.email} `
+  );
+
+  const formattedRideDate = moment
+    .tz(ride.pickupTimeAndDate, process.env.TIMEZONE)
+    .format(process.env.DATE_FORMAT);
+
+  await sendEmail({
+    to: driver.email,
+    subject: `Hills Carpal needs feedback from your ride for ${ride.client.name}`,
+    html: `
+      <p>Hi ${driver.given_name || driver.nickname || ''},</p>
+
+      <p>
+        We still haven't received feedback for the ride you gave to ${
+          ride.client.name
+        } at
+        ${formattedRideDate}. Please complete the feedback form and mark the ride as completed at
+        <a href="${process.env.EXTERNAL_URL}/driver/rides/${
+      ride.id
+    }/poll">click here</a>.
+      </p>
+
+      <p>
+        Thanks,<br>
+        Hills Carpal
+      </p>
+    `,
+  });
+}
