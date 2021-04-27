4c509584bf98d8670615982051ec610228632ba5
Alex Gilleran <AlexGilleran@users.noreply.github.com>
2021-04-27 19:56:45 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,6 @@ module.exports = withPlugins([
         REACT_APP_AUTH_METADATA_ROLE: 'roles',
         REACT_APP_UNSAFE_GOD_MODE:
           process.env.REACT_APP_UNSAFE_GOD_MODE || false,
-        EXTERNAL_URL: process.env.EXTERNAL_URL || 'https://ride.carpal.org.au',
         DATE_FORMAT: process.env.DATE_FORMAT || 'DD/MM/YYYY hh:mma',
         TIMEZONE: process.env.TIMEZONE || 'Australia/Sydney',
         REMINDER_DIFFERENCE_DAYS: process.env.REMINDER_DIFFERENCE_DAYS || '5',
